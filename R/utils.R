396d78f7413704600087349fcdc8310b2cf84239
Patrik <Patrikios@users.noreply.github.com>
2022-06-08 12:07:39 +0200

    Update utils.R
    Somehow in the previous commit the 'check_col_mode' function got omitted. This commit corrects it.

--- R/utils.R
+++ R/utils.R
@@ -12,3 +12,24 @@ globalVariables(c("rn",
                   "endObjects",
                   "..colsToKeep")
                 )
+
+#' check_col_mode
+#'
+#' Help function for checking if a data.frame/data.table column has a numeric mode.
+#' Is used for warning is true, because in the next step will try to convert the colum to a Date class.
+#' It is a user conscious warnings systemi in order to avoid user side mistakes.
+#'
+#' @param df data.frame/data.table
+#' @param col df column, a charactert, that will be checked for the mode 'numeric'
+#'
+#' @noRd
+#'
+check_col_mode <- function(df, col_name) {
+  if (mode(df[[col_name]]) == "numeric") {
+    # warning or could be a little less scary with 'message'
+    warning(sprintf(
+      "Be aware: Column '%s' is not a date class variable, hence trying to convert into a date. Originally, the column is of mode 'numeric'. Was counting in ideal case with non-numeric mode of the column (e.g. 'character' or 'POSIXct')",
+      col_name
+    ))
+  }
+}
